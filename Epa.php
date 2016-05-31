e920f076b7d9c3b7705650425a0355664d003f75
el-ma <eyupcol@yahoo.com>
2016-05-31 12:54:27 +0300

    Update Epa.php

--- Epa.php
+++ Epa.php
@@ -7,5 +7,18 @@
 
 class Epa
 {
+  private $lang = array();
+  
+  public function __construct()
+  {
+    
+  }
+  
+  public function set_lang_source()
+  {
+    $this->lang = array('next_page'=>'Next Page');
+  }
   
 }
+
+?>
